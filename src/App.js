824554c34b120a6a125be8b33d80809172343826
Rishita Srivastava <rixsri.13@gmail.com>
2023-04-16 00:37:15 +0530

    register page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import {BrowserRouter as Router } from 'react-router-dom';
+import {Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Register from './pages/Register/Register';
 function App() {
   return (
     <Router>
     <div className="App">
-      <Navbar/>
+    <Routes>
+         <Route path="/" element={<Home/>}/>
+         <Route path="/register" element={<Register/>}/>
+        </Routes>
     </div>
     </Router>
   );
