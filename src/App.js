4cd34c17d0fe606dcc3158df2ac5997a4567c809
Rishita Srivastava <rixsri.13@gmail.com>
2023-04-15 20:57:32 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-
+import Navbar from './components/Navbar/Navbar';
+import {BrowserRouter as Router } from 'react-router-dom';
 function App() {
   return (
+    <Router>
     <div className="App">
-      React App
+      <Navbar/>
     </div>
+    </Router>
   );
 }
 
