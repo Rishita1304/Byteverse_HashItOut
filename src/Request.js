c27ecb75feeb3b3e5e774c28e6ef8866e0ad15b4
Rishita Srivastava <rishitasrivastava04@gmail.com>
2024-09-13 23:51:05 +0530

    spinner added

--- src/Request.js
+++ src/Request.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const publicRequest = axios.create({
-    baseURL: "http://localhost:8002/api/",
-    // baseURL: "https://updrivenbackend.onrender.com/api/",
+    // baseURL: "http://localhost:8002/api/",
+    baseURL: "https://updrivenbackend.onrender.com/api/",
   });
